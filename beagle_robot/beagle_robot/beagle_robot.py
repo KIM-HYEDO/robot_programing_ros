2b834c5c9c354c99168f0b6ab6cd5d1fb9e69d0a
SeungWon-Jang17 <qdlmlbp@kw.ac.kr>
2023-06-14 23:21:29 +0900

    update 0614

--- beagle_robot/beagle_robot/beagle_robot.py
+++ beagle_robot/beagle_robot/beagle_robot.py
@@ -13,9 +13,9 @@ from roboid import *
 class BeagleRobot(Node):
 
     def __init__(self):
-        super().__init__('beagle_robot')
+        super().__init__('beagle_robot') # 노드 이름 저장
         self.beagle = Beagle()
-        qos_profile = QoSProfile(depth=10)
+        qos_profile = QoSProfile(depth=10) # 퍼블리시 할 데이터를 버퍼에 10개 저장
 
         self.moter_go_publisher = self.create_publisher(
             MoterControl, '/moter_control', qos_profile)
@@ -23,6 +23,7 @@ class BeagleRobot(Node):
             BuzzerControl, '/buzzer_control', qos_profile)
         self.servo_publisher = self.create_publisher(
             ServoControl, '/servo_control', qos_profile)
+        
         self.lidar_subscriber = self.create_subscription(
             LidarData,
             '/lidar_data',
@@ -33,6 +34,7 @@ class BeagleRobot(Node):
             '/camera_detection',
             self.camera_callback,
             qos_profile)
+
 
     def ConnectBeagle(self):
         self.beagle = Beagle()
@@ -54,8 +56,15 @@ class BeagleRobot(Node):
         self.beagle.lidar_chart()
         wait(-1)
 
-    def MoterControl(self, left_speed, right_speed, sec):
-        self.beagle.wheels(left_speed, right_speed)
+    def MoterControl(self, moter_l, moter_r, sec):#request와 response의 형태로
+        self.beagle.wheels(moter_l, moter_r)
+        
+        encoderl = self.beagle.left_encoder()
+        encoderr = self.beagle.right_encoder()
+        
+        responseL = 0#(requestL)
+        responseR = 0#(requestR)
+
         wait(sec * 1000)
 
     def ServoControl(self, num, degree, speed):
@@ -71,16 +80,18 @@ class BeagleRobot(Node):
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init(args=args) # 초기화
     node = BeagleRobot()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         node.get_logger().info('Keyboard Interrupt (SIGINT)')
     finally:
-        node.destroy_node()
-        rclpy.shutdown()
+        node.destroy_node() # 노드 소멸
+        rclpy.shutdown() # 함수 종료
 
 
 if __name__ == '__main__':
     main()
+
+#서비스로 엔코더값 보내기
